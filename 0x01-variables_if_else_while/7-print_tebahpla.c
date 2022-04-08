2cd0d4e4f60841f3b6db2b22ca9b2a8d3fb641e8
enigmire <meathical@gmail.com>
2022-04-08 14:51:25 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+{
+putchar('\n');
+}
+return (0);
+}
