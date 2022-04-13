477f389cb71f2da59ef1e5bce258e8b0ffcb3c29
enigmire <meathical@gmail.com>
2022-04-13 13:43:17 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - check for alphacase letters
+* @r : character to check the case
+* Return: 0 or 1
+*/
+
+int print_sign(int n)
+
+{
+return ((n > 0) && printf(+));
+return ((n = 0) && printf(0));
+return ((n > -1) && printf(-));
+}
