3a5035f7a42ec570dab65a5b4958d7a9836a8005
enigmire <meathical@gmail.com>
2022-04-13 12:51:14 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* _isalpha - check for lowercase letters
+* _isalpha - check for alphacase letters
 * @r : character to check the case
+* @z : character to check the case
 * Return: 0 or 1
 */
 
@@ -11,3 +12,7 @@ int _isalpha(int r)
 {
 return (r >= 97 && r <= 122);
 }
+int _islower(int z)
+{
+return (z >=97 && r <= 122);
+}  
