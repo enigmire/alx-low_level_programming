cf8968c036a490d181279216d4701baa4324b55d
enigmire <meathical@gmail.com>
2022-04-13 13:28:08 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,11 @@
 /**
 * _isalpha - check for alphacase letters
 * @r : character to check the case
-* @z : character to check the case
 * Return: 0 or 1
 */
 
 int _isalpha(int r)
 
 {
-  return ((r >= 97 && r <= 122) || (r >= 65 && r <= 90));
+return ((r >= 97 && r <= 122) || (r >= 65 && r <= 90));
 }
