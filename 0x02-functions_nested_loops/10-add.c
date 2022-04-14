0f148e93050618c24e5ca2050cefcb36b78406aa
enigmire <meathical@gmail.com>
2022-04-14 05:52:15 -0700

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
-* add(int, int) - add two integers togerther
+* add - add two integers togerther
 * @i: int type number
 * @j: int type number
-* Return - result
+* Return - 0
 */
 
 int add(int i, int j)
 {
-int result;
-i = 3;
-j = 4;
-
-result = i + j;
-return (result);
+return (i + j);
 }
