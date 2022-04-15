f5a82e7b8720017f2c1f43e8a70f123a16be66ff
enigmire <meathical@gmail.com>
2022-04-15 08:31:21 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - check for uppercase letters
+* @r : character to check the case
+* Return: 0 or 1
+*/
+
+int _isupper(int r)
+
+{
+return (r >= 97 && r <= 122);
+}
