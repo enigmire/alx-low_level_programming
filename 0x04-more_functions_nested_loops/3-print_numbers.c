944bca803185ce9ad1e1f905d70bc07acf7483f1
enigmire <meathical@gmail.com>
2022-04-15 10:01:43 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* print_most_numbers - a function that prints the numbers, from 0 to 9.
-* Do not print 2 and 4.
-* followed by a new line.
-* Return: Always 0
+* print_numbers - prints numbers 0-9
+* Return: void
 */
 
-void print_most_numbers(void)
-  
+void print_numbers(void)
 {
-int i = '0';
-
-for (; i <= '9'; i++)
+int c;
+for (c = '0'; c <= '9'; c++)
 {
-if (i != '2' && i != '4')
-_putchar(i);
+_putchar(c);
 }
 _putchar('\n');
 }
