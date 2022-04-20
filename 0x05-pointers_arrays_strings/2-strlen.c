c2b2f2e1e450bdc34aed0b5a2943cb6fbf44357f
enigmire <meathical@gmail.com>
2022-04-20 14:16:34 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of string
+* @a: integer to swap
+* @b: integer to swap
+* @temp:
+* Return: 0
+*/
+
+int _strlen(char *s)
+{
+_strlen(char *s);
+}
