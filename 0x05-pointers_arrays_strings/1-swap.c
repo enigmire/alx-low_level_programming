f58b23bd558556f94aa31f7da2f20cfe9bb29636
enigmire <meathical@gmail.com>
2022-04-20 07:03:57 -0700

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* swap_int - sway the values of two integers
+* @a: integer to swap
+* @b: integer to swap
+* Return: 0
+*/
+
+void swap_int(int *a, int *b)
+{
+a = &b;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #define MAIN_H_INCLUDED
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
