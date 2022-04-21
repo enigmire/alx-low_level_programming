4d8732f2fa121e4efc667a6a508e5e4926e6284d
enigmire <meathical@gmail.com>
2022-04-21 13:56:50 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @s: pointer to variable s
+* Return: 0
+*/
+
+void _puts(char *str)
+{
+int i = 0;
+while (s[i])
+i++;
+_putchar('\n');
+return (i);
+}
