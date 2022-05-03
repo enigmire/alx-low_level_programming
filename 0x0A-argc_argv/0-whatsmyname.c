ef5e54efdc76e39c7c7919637aa364a6f9f93988
enigmire <meathical@gmail.com>
2022-05-03 06:59:50 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* @argc: Variable counter
+* @argv: Array variable
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+int i = argc - argc;
+printf("%s\n", argv[i]);
+return (0);
+}
