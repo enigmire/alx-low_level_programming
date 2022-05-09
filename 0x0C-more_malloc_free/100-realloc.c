7dc68c47aafaee6f8e5e0206d40f5f2dfb6a625f
enigmire <meathical@gmail.com>
2022-05-09 13:17:01 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,8 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     {      
       free(ptr);      
       return (NULL);      
-    }
-  
+    }  
   else if (new_size == old_size)    
     return (ptr);
   
