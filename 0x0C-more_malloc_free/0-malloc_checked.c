e59eee3a634f39aeb0e2ce2bd6ffad585f9975b6
enigmire <meathical@gmail.com>
2022-05-09 14:22:42 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,42 +2,17 @@
 #include <stdlib.h>
 
 /**
-* string_nconcat - concatenates two strings.
-* @s1: first string to copy
-* @s2: second string to copy
-* @n: number of bytes of s2 to copy
+* malloc_checked - allocates memory using malloc
+* @b: memory to allocate
 *
-* Return: char pointer to newly allocated place in memory
+* Return: pointer to allocated memory or normal process termination if error
 */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-unsigned int i, j, k;
-char *s;
-if (s1 == NULL)
-i = 0;
-else
-{
-for (i = 0; s1[i]; i++)
-;
-}
-if (s2 == NULL)
-j = 0;
-else
-{
-for (j = 0; s2[j]; j++)
-;
-}
-if (j > n)
-j = n;
-s = malloc(sizeof(char) * (i + j + 1));
-
-if (s == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-s[k] = s1[k];
-for (k = 0; k < j; k++)
-s[k + i] = s2[k];
-s[i + j] = '\0';
-return (s);
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
 }
