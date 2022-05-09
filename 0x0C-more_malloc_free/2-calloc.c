263901e45abf3fb9f4f73373a3117b4982a3ca8d
enigmire <meathical@gmail.com>
2022-05-09 14:01:17 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,33 +11,18 @@
 
 int *array_range(int min, int max)
 {
-  
-  int i, l;
-  
-  int *a;
-  
+int i, l;
+int *a;
 
-  
-  if (min > max)
-    
-    return (NULL);
-  
-  l = max - min + 1;
-  
-  a = malloc(sizeof(int) * l);
-  
-  if (a == NULL)
-    
-    return (NULL);
-  
-  for (i = 0; i < l; i++, min++)
-    
-    {
-      
-      a[i] = min;
-      
-    }
-  
-  return (a);
-  
+if (min > max)
+return (NULL);
+l = max - min + 1;
+a = malloc(sizeof(int) * l);
+if (a == NULL)
+return (NULL);
+for (i = 0; i < l; i++, min++)
+{
+a[i] = min;
+}
+return (a);
 }
