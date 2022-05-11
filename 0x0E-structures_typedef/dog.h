c16dc0f959a6e2704cccb381b5bb67ff7e3cf397
enigmire <meathical@gmail.com>
2022-05-11 04:18:51 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+  struct dog
+  {
+    char *name;
+    float age;
+    char *owner;
+  }
+  return (0);
+}
